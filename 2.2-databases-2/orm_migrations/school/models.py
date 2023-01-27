fd95186d5cf945909b72aad74db3d6140e6339ad
mikhaylovvl <mikhayl0vvl@yandex.ru>
2023-01-27 11:14:25 +0300

    Выполнил задание Работа с ORM Миграции

--- 2.2-databases-2/orm_migrations/school/models.py
+++ 2.2-databases-2/orm_migrations/school/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Teacher(models.Model):
-    name = models.CharField(max_length=30, verbose_name='Имя')
+    name = models.CharField(max_length=30, verbose_name='Имя учителя')
     subject = models.CharField(max_length=10, verbose_name='Предмет')
 
     class Meta:
@@ -14,13 +14,15 @@ class Teacher(models.Model):
 
 
 class Student(models.Model):
-    name = models.CharField(max_length=30, verbose_name='Имя')
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30, verbose_name='Имя студента')
+    # teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    teachers = models.ManyToManyField(Teacher, related_name='students')
     group = models.CharField(max_length=10, verbose_name='Класс')
 
     class Meta:
         verbose_name = 'Ученик'
         verbose_name_plural = 'Ученики'
+        ordering = ['group']
 
     def __str__(self):
         return self.name
